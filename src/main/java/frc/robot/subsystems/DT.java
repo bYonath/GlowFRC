91d42d94df23ae54fc0621a6b579d2bf595a5582
Tobi <tobisonnier@gmail.com>
2024-02-21 07:45:10 -0600

    Update to the Drive Train code
    - Added more support for different motors and edited to code to better convey its purpose.
    - Added more comments to the code.

--- src/main/java/frc/robot/subsystems/DT.java
+++ src/main/java/frc/robot/subsystems/DT.java
@@ -18,7 +18,6 @@ import edu.wpi.first.wpilibj.XboxController;
 public class DT extends SubsystemBase {
   public DifferentialDrive driveTrain;
   private CommandXboxController xb1 = new CommandXboxController(0);
-
   public WPI_VictorSPX frontRight;
   public WPI_VictorSPX frontLeft;
   public WPI_VictorSPX backRight;
@@ -26,13 +25,16 @@ public class DT extends SubsystemBase {
   public WPI_VictorSPX intakeMotor;
   public static CANSparkMax shooterRight;
   public static CANSparkMax shooterLeft;
+  public WPI_VictorSPX transferMotor;
   public Trigger rightTrigger = xb1.rightTrigger();
-  public Trigger lefTrigger = xb1.leftTrigger();
+  public Trigger rightBumper = xb1.rightBumper();
+  public Trigger leftTrigger = xb1.leftTrigger();
   public Trigger aButton = xb1.a();
+  public Trigger leftBumper = xb1.leftBumper();
   /* Creates a new Motor Object */
   /** Creates a new Drivetrain. (Note the args passed through are getting the axis of the controller joystick) */
   public DT() {
-
+    // Initializes all the motors. 
     frontRight = new WPI_VictorSPX(0);
     frontLeft = new WPI_VictorSPX(1);
     backRight = new WPI_VictorSPX(2);
@@ -40,6 +42,8 @@ public class DT extends SubsystemBase {
     intakeMotor = new WPI_VictorSPX(4);
     shooterRight = new CANSparkMax(5, MotorType.kBrushless);
     shooterLeft = new CANSparkMax(6, MotorType.kBrushless);
+    transferMotor = new WPI_VictorSPX(7);
+    // Initializes the drive train as a new instance of the DifferentialDrive class
     driveTrain = new DifferentialDrive(
       (double output) -> {
         frontLeft.set(output);
@@ -54,25 +58,40 @@ public class DT extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    driveTrain.arcadeDrive(xb1.getRawAxis(1), xb1.getRightX());
-    if(aButton.getAsBoolean() == true){
+
+    // Using the tankDrive (or arcadeDrive) method of the driveTrain class and flipping the right side inputs to fit driver's tastes and have both sides move the same way
+    //driveTrain.tankDrive(xb1.getRightY() * -1, xb1.getLeftY());
+    driveTrain.arcadeDrive(xb1.getRightX(), xb1.getLeftY());
+    if(leftBumper.getAsBoolean() == true){
       intakeMotor.set(-1);
-    } else if(aButton.getAsBoolean() == false){
+      transferMotor.set(-1);
+    } else if(leftBumper.getAsBoolean() == false){
       intakeMotor.set(0);
     }
-    if(rightTrigger.getAsBoolean() == true){
+    if(leftTrigger.getAsBoolean() == true){
+      intakeMotor.set(-1);
+      transferMotor.set(-1);
+    } else if(leftTrigger.getAsBoolean() == false){
+      intakeMotor.set(0);
+      transferMotor.set(0);
+    }
+    if(rightBumper.getAsBoolean() == true){
       shooterRight.set(1);
       shooterLeft.set(1);
-    }else if(rightTrigger.getAsBoolean() == false){
+      transferMotor.set(-1);
+    } else if (rightBumper.getAsBoolean() == false){
       shooterRight.set(0);
       shooterLeft.set(0);
+      transferMotor.set(0);
     }
-    if(lefTrigger.getAsBoolean() == true){
+    if(rightTrigger.getAsBoolean() == true){
       shooterRight.set(-1);
       shooterLeft.set(-1);
-    } else if(lefTrigger.getAsBoolean() == false){
+      transferMotor.set(-1);
+    } else if(rightTrigger.getAsBoolean() == false){
       shooterRight.set(0);
       shooterLeft.set(0);
+      transferMotor.set(0);
     }
   }
 }
